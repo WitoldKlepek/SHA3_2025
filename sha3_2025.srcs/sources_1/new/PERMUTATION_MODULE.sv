9e00023ba4f5999e81cbe88172207a46c4e8ac17
unknown <klepek@student.agh.edu.pl>
2025-04-25 23:16:31 +0200

    control commit

--- sha3_2025.srcs/sources_1/new/PERMUTATION_MODULE.sv
+++ sha3_2025.srcs/sources_1/new/PERMUTATION_MODULE.sv
@@ -52,7 +52,7 @@ logic load_word_without_req_sig;
 typedef enum {  INIT,
                 LOAD_NEW_WORD,
                 PROCESSING,
-                REQ_FOR_NEW_WORD,
+                //REQ_FOR_NEW_WORD,
                 END_OF_MESSAGE_HASH_READY,
                 NO_WORK} permutation_fsm;
 
@@ -86,21 +86,23 @@ always_ff @(posedge CLK, posedge A_RST) begin
                 LOAD_NEW_WORD:
                     state   <=  PROCESSING;
                 PROCESSING:
-                    if(permutation_counter == `PERMUTATION_NUMBER - 2)
+                    if(permutation_counter == `PERMUTATION_NUMBER - 1)
                         if(LAST_WORD_FROM_PADDING)
                             state   <= END_OF_MESSAGE_HASH_READY;
                         else
-                            state   <= REQ_FOR_NEW_WORD;
+                            state   <= LOAD_NEW_WORD;
                     else
                         state   <= PROCESSING;
-                REQ_FOR_NEW_WORD:
-                    state   <=  LOAD_NEW_WORD;
+                //REQ_FOR_NEW_WORD:
+                    //state   <=  LOAD_NEW_WORD;
                 END_OF_MESSAGE_HASH_READY:
-                    if()
-                    
+                    if(WORD_WAITING)
+                        state   <=  LOAD_NEW_WORD;
                     else
                         state   <=  NO_WORK;
                 NO_WORK:
+                    if(WORD_WAITING)
+                        state   <=  LOAD_NEW_WORD;
                 default:
             endcase
         
@@ -118,15 +120,27 @@ always_ff @(posedge CLK, posedge A_RST) begin
                 LOAD_NEW_WORD:
                     permutation_counter <=  permutation_counter + 1;
                 PROCESSING:
-                    permutation_counter <=  permutation_counter + 1;
-                REQ_FOR_NEW_WORD:
-                    permutation_counter <=  0;
+                    if(permutation_counter == `PERMUTATION_NUMBER - 1)
+                        permutation_counter <=  0;
+                    else
+                        permutation_counter <=  permutation_counter + 1;
+                //REQ_FOR_NEW_WORD:
+                    //permutation_counter <=  0;
                 END_OF_MESSAGE_HASH_READY:
+                    permutation_counter <=  0;    
                 NO_WORK:
                 default:
             endcase
         
 end
+
+assign READ_REQ = (state == LOAD_NEW_WORD)? 1'b1 : 1'b0;
+//pobranie wiadomosci
+
+
+assign HASH_VALID = (state == END_OF_MESSAGE_HASH_READY)? 1'b1 : 1'b0;
+//wyjscie
+
 
 ROUND_CONSTANT_CONVERTER CONV1(
     .COUNTER(permutation_counter),
